22c13b03ec13b78cc1d3d901723e342d7540cada
Thalita <thalitafdrumond@gmail.com>
2014-09-03 10:55:01 -0300

    Correction in SuffixTree.py
    - nleaves was counting number of children of a given node. Corrected the name
    and added leaf counting to the end of the constructor.
    - added a few methods to get or check for leaf and internal nodes
    - added pprint_tree as a method of SuffixTree
    - added a method to GeneralizedSuffixTree to allow checking to which strings
    belong the suffixes reachable from a given node.

--- SuffixTree.py
+++ SuffixTree.py
@@ -34,13 +34,14 @@ def split_edge(edge, suffix, suffix_tree):
     new_node = Node()#suffix.src_node_idx
     suffix_tree.nodes.append(new_node)
     new_node_idx = len(suffix_tree.nodes) - 1
+    suffix_tree.nchildren.append(1)
     #alloc new edge
     new_edge = Edge(new_node_idx, edge.dst_node_idx, edge.first_char_idx + len(suffix), edge.last_char_idx)
     suffix_tree.insert_edge(new_edge)
-    suffix_tree.nleaves.append(1)
     #shorten existing edge
     edge.last_char_idx = edge.first_char_idx + len(suffix) - 1
     edge.dst_node_idx = new_node_idx
+    suffix_tree.edge_by_dst[new_node_idx] = edge
     #save new node's depth
     suffix_tree.depths.append(suffix_tree.depths[edge.src_node_idx] + len(edge))
     return new_node_idx
@@ -91,7 +92,7 @@ class SuffixTree:
 
         self.nodes = [Node()]
         self.depths = [0]
-        self.nleaves = [0]
+        self.nchildren = [0]
 
         self.edge_lookup = {} #edge_source_node_first_char_dict
         self.edge_by_dst = {}
@@ -100,15 +101,54 @@ class SuffixTree:
         for j in range(len(string)):
             add_prefix(j, self.active_point, self)
 
+        self.nleaves = [0 for i in range(len(self.nodes))]
+        self.__leaf_count__()
+
     def insert_edge(self, edge):
         self.edge_lookup[edge.src_node_idx, self.string[edge.first_char_idx]] = edge
         self.edge_by_dst[edge.dst_node_idx] = edge
 
     def remove_edge(self, edge):
         del self.edge_lookup[edge.src_node_idx, self.string[edge.first_char_idx]]
+        del self.edge_by_dst[edge.dst_node_idx]
+
+    def internal_nodes_idx(self):
+        return [idx for idx, x in enumerate(self.nchildren) if x !=0]
+
+    def leaf_nodes_idx(self):
+        return [idx for idx, x in enumerate(self.nchildren) if x ==0]
+
+    def path_to_node(self, node):
+        label = self.string[0:0]
+        while node > 0:
+            edge = self.edge_by_dst[node]
+            label = self.string[edge.first_char_idx:edge.last_char_idx+1] + label
+            node = self.edge_by_dst[node].src_node_idx
+        return label
+
+    def is_leaf(self, node):
+        return bool(self.nchildren[node]==0)
+
     def __repr__(self):
         return pprint_tree(self)
-        
+
+    def pprint_tree(self):
+        print pprint_tree(self)
+
+    def __leaf_count__(self, start_node=0):
+        count = 0
+        for c in self.alphabet:
+            try:
+                edge = self.edge_lookup[start_node, c]
+                if not self.is_leaf(edge.dst_node_idx):
+                    count += self.__leaf_count__(edge.dst_node_idx)
+                else:
+                    count += 1
+            except KeyError:
+                pass
+        self.nleaves[start_node] = count
+        return count
+
 class GeneralizedSuffixTree(SuffixTree):
     def __init__(self, strings):
         # create cat string
@@ -116,8 +156,8 @@ class GeneralizedSuffixTree(SuffixTree):
         self.strings = strings
         terminators = set()
         alphabet = set()
-        string = Strings[0][0:0] #generic way of geting a empty object of the same type of Strings items
-        for s in Strings:
+        string = strings[0][0:0] #generic way of geting a empty object of the same type of Strings items
+        for s in strings:
             string += s
             terminators.add(s[-1])
             for l in set(s[0:-1]):
@@ -129,8 +169,7 @@ class GeneralizedSuffixTree(SuffixTree):
         SuffixTree.__init__(self,string)
 
         # fix depth and leaf edges labels
-        leaf_nodes_idx = [idx for idx, x in enumerate(self.nleaves) if x ==0]
-        for node in leaf_nodes_idx:
+        for node in self.leaf_nodes_idx():
             edge = self.edge_by_dst[node]
             s = self.string[edge.first_char_idx:edge.last_char_idx+1]
             for idx, char in enumerate(s):
@@ -139,10 +178,51 @@ class GeneralizedSuffixTree(SuffixTree):
                     self.depths[node] -= (edge.last_char_idx-idx)
                     self.edge_by_dst[node].last_char_idx = idx
                     self.edge_lookup[(edge.src_node_idx,self.string[edge.first_char_idx])].last_char_idx = idx
-
                     break
 
+    def strings_from_node(self, node, ids=set()):
+        for c in self.alphabet:
+            try:
+                edge = self.edge_lookup[node, c]
+                if not self.is_leaf(edge.dst_node_idx):
+                    ids.update(self.strings_from_node(edge.dst_node_idx, ids))
+                else:
+                    corrected_idx = edge.last_char_idx
+                    for s_id, s in enumerate(self.strings):
+                        if corrected_idx-len(s)>=0:
+                            corrected_idx -= len(s)
+                        else:
+                            break
+                    ids.add(s_id)
+            except KeyError:
+                pass
+        return ids
 
+def pprint_tree(suffix_tree, start_node=0, string = ''):
+
+    string += 'N%d[d%dl%d]' % (start_node, suffix_tree.depths[start_node], suffix_tree.nleaves[start_node])
+    if suffix_tree.nodes[start_node].suffix_link > 0:
+        string += '(->' + str(suffix_tree.nodes[start_node].suffix_link) + ')'
+
+    init = string.rfind('\n')+1 # if no \n found, init will be 0
+    depth = len(string)-init
+
+    first_edge = True
+    for c in suffix_tree.alphabet:
+        try:
+            edge = suffix_tree.edge_lookup[start_node, c]
+            string += '|--'
+            string += str(suffix_tree.string[edge.first_char_idx:edge.last_char_idx+1])
+            string += '--'
+            string = pprint_tree (suffix_tree, edge.dst_node_idx, string)
+            first_edge = False
+            string += '\n'
+            string += ' '*(depth)
+
+        except KeyError:
+            pass
+
+    return string
 
 
 def add_prefix(last_char_idx, active_point, suffix_tree):
@@ -165,8 +245,8 @@ def add_prefix(last_char_idx, active_point, suffix_tree):
         new_node_idx = len(suffix_tree.nodes) - 1
         new_edge = Edge(parent_node_idx, new_node_idx, last_char_idx, POSITIVE_INFINITY)#src,dest,1st char,last char
         suffix_tree.insert_edge(new_edge)
-        suffix_tree.nleaves[parent_node_idx] += 1
-        suffix_tree.nleaves.append(0)
+        suffix_tree.nchildren[parent_node_idx] += 1
+        suffix_tree.nchildren.append(0)
         suffix_tree.depths.append(suffix_tree.depths[parent_node_idx] + len(new_edge))
         #add suffix link if it is not the first edge split in this call
         if last_parent_node_idx > 0:
@@ -262,31 +342,7 @@ def show_node(suffix_tree, node_idx):
             pass
     print str(node_idx) + ' -> ' + str(suffix_tree.nodes[node_idx])
 
-def pprint_tree(suffix_tree, start_node=0, depth=0, string = ''):
 
-    string += 'N%d[d%dl%d]' % (start_node,suffix_tree.depths[start_node],suffix_tree.nleaves[start_node])
-    if suffix_tree.nodes[start_node].suffix_link > 0:
-        string += '(->' + str(suffix_tree.nodes[start_node].suffix_link) + ')'
-
-    init = string.rfind('\n')+1 # if no \n found, init will be 0
-    depth = len(string)-init
-
-    first_edge = True
-    for c in suffix_tree.alphabet:
-        try:
-            edge = suffix_tree.edge_lookup[start_node, c]
-            string += '|--'
-            string += str(suffix_tree.string[edge.first_char_idx:edge.last_char_idx+1])
-            string += '--'
-            string = pprint_tree (suffix_tree, edge.dst_node_idx, depth*(not first_edge), string)
-            first_edge = False
-            string += '\n'
-            string += ' '*(depth)
-
-        except KeyError:
-            pass
-
-    return string
 
 
 if __name__=='__main__':
@@ -309,3 +365,7 @@ if __name__=='__main__':
     #is_valid = is_valid_suffix_tree(suffix_tree)
     #print 'is_valid_suffix_tree:', is_valid
     print pprint_tree(suffix_tree)
+
+    print suffix_tree.path_to_node(15)
+
+    print suffix_tree.strings_from_node(17)
